363621ba6cb8c70a880e72cea68f5e11fe0eeee2
TakeTheLoweRoad <samanthalowe2010@gmail.com>
2021-04-12 16:07:37 -0400

    Including Product model, controller, html, etc, and an updated home page

---
+++ PremoPizza/src/main/java/Premo/Repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package Premo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Premo.Model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
